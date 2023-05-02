2d24b42262183434e1bf1c2a0a1def0405e254e6
Shoham <medinashoham@gmail.com>
2023-05-02 11:32:32 -0600

    se crea clase Herenciaej

---
+++ src/main/java/Interfaces/Becable.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface Becable {
+    public abstract Boolean becable();
+
+}
---
+++ src/main/java/Interfaces/InterFacesApp.java
@@ -0,0 +1,40 @@
+package Interfaces;
+
+public class InterFacesApp {
+        public static void main(String[] args) {
+            Estudiante estudiante = new Estudiante(
+                    "Juan",
+                    "Perez",
+                    20,
+                    "UNLP",
+                    "Informatica",
+                    "2do"
+            );
+
+            Empleado empleado = new Empleado(
+                    "Pedro",
+                    "Gomez",
+                    30,
+                    "Google",
+                    "Senior Programmer",
+                    "150000"
+            );
+
+            EstudianteTrabajador estudianteTrabajador = new EstudianteTrabajador(
+                    "Maria",
+                    "Gonzalez",
+                    25,
+                    "UNLP",
+                    "Informatica",
+                    "9no",
+                    "Facebook",
+                    "Junior Programmer",
+                    "100000"
+            );
+
+            System.out.println(estudiante.toString());
+            System.out.println(empleado.toString());
+            System.out.println(estudianteTrabajador.toString());
+        }
+    }
+
